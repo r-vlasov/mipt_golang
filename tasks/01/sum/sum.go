d07d9506d36e7919a10ec4c2c71320a01a9b1c2e
Roman <roman@kali.linux-kali>
2022-04-23 19:58:00 -0400

    +1

--- tasks/01/sum/sum.go
+++ tasks/01/sum/sum.go
@@ -1,6 +1,14 @@
 package sum
 
+
+func add(x, y int) int {
+	return x + y;
+}
+
 func Sum(values []int) int {
-	// Your implementation
-	return 0
+	var sum int;
+	for i := 0; i < len(values); i++ {
+		sum = add(sum, values[i])
+	}
+	return sum
 }
