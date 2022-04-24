44f4041ce23aa584f6446a6ffca64a97e1516d29
Roman <roman@kali.linux-kali>
2022-04-24 13:46:04 -0400

    +4

--- tasks/02/timepng/timepng.go
+++ tasks/02/timepng/timepng.go
@@ -2,27 +2,56 @@ package timepng
 
 import (
 	"image"
+	"fmt"
 	"image/color"
+	"image/png"
 	"io"
 	"time"
 )
 
 // TimePNG записывает в `out` картинку в формате png с текущим временем
 func TimePNG(out io.Writer, t time.Time, c color.Color, scale int) {
-	// TODO: Implement me
+	img := buildTimeImage(t, c, scale)
+	png.Encode(out, img)
 }
 
 // buildTimeImage создает новое изображение с временем `t`
 func buildTimeImage(t time.Time, c color.Color, scale int) *image.RGBA {
-	// TODO: Implement me
-	return nil
+	t_arr := fmt.Sprintf("%02d:%02d", t.Hour(), t.Minute())
+	x_size := scale * 3
+	y_size := scale * 5
+	img := image.NewRGBA(image.Rect(0, 0, (x_size + scale) * len(t_arr) - scale, y_size))
+
+	mask := []int{}
+	for y := 0; y < 5; y++ {
+		for l := 0; l < len(t_arr); l++ {
+			ch := rune(t_arr[l])
+			if l != 0 {
+				mask = append(mask, 0)
+			}
+			for x := 0; x < 3; x++ {
+				mask = append(mask, nums[ch][y * 3 + x])
+			}
+		}
+	}
+	fillWithMask(img, mask, c, scale)
+	return img
 }
 
 // fillWithMask заполняет изображение `img` цветом `c` по маске `mask`. Маска `mask`
 // должна иметь пропорциональные размеры `img` с учетом фактора `scale`
 // NOTE: Так как это вспомогательная функция, можно считать, что mask имеет размер (3x5)
 func fillWithMask(img *image.RGBA, mask []int, c color.Color, scale int) {
-	// TODO: implement me
+	y_size := scale * 5
+	for x := 0; x < len(mask) * scale / 5; x++ {
+		i := x / scale
+		for y := 0; y < y_size; y++ {
+			j := y / scale
+			if mask[j * len(mask) / 5 + i] == 1 {
+				img.Set(x, y, c)
+			}
+		}
+	}
 }
 
 var nums = map[rune][]int{
@@ -101,7 +130,6 @@ var nums = map[rune][]int{
 		0, 1, 0,
 		0, 0, 0,
 		0, 1, 0,
-		0, 0, 0,
 		0, 0, 0,
 	},
 }
