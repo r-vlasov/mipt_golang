23e8b558a264606da49ef1d7a23b53a76ecee1cd
Roman <roman@kali.linux-kali>
2022-04-25 20:24:02 -0400

    +3

--- tasks/02/urlshortener/shortener.go
+++ tasks/02/urlshortener/shortener.go
@@ -2,24 +2,50 @@ package urlshortener
 
 import (
 	"net/http"
+	"net/url"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"github.com/go-chi/chi"
 )
 
 type URLShortener struct {
-	// TODO: More fields?
 	addr string
+	mapping map[string]string
 }
 
 func NewShortener(addr string) *URLShortener {
 	return &URLShortener{
-		// TODO: Initialization for more fields?
 		addr: addr,
+		mapping: map[string]string{},
 	}
 }
 
 func (s *URLShortener) HandleSave(rw http.ResponseWriter, req *http.Request) {
-	// TODO: Implement me
+	raw_url := req.URL.Query().Get("u")
+	_, err := url.Parse(raw_url)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+	}
+	hasher := md5.New()
+	hasher.Write([]byte(raw_url))
+	mapped_path := hex.EncodeToString(hasher.Sum(nil))
+	if _, ok := s.mapping[mapped_path]; ok {
+		rw.WriteHeader(http.StatusInternalServerError)
+	} else {
+		s.mapping[mapped_path] = raw_url
+		_, err := fmt.Fprintf(rw, "%s/%s", s.addr, mapped_path)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+		}
+	}
 }
 
 func (s *URLShortener) HandleExpand(rw http.ResponseWriter, req *http.Request) {
-	// TODO: Implement me
+	r_url := chi.URLParam(req, "key")
+	if mapped_path, ok := s.mapping[r_url]; ok {
+		http.Redirect(rw, req, mapped_path, http.StatusMovedPermanently)
+	} else {
+		rw.WriteHeader(http.StatusNotFound)
+	}
 }
