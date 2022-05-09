7a1c99ef3755141634770090b4c62152cea0bb46
Roman <roman@kali.linux-kali>
2022-05-09 16:49:26 -0400

    2

--- tasks/04/middleware/recover.go
+++ tasks/04/middleware/recover.go
@@ -6,5 +6,17 @@ import (
 )
 
 func Recover(logger *log.Logger) func(http.Handler) http.Handler {
-	return nil
+   return func(h http.Handler) http.Handler {
+       return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+           defer func() {
+               r := recover()
+               if r != nil {
+                   logger.Printf("[ERROR] Panic caught: %v", r)
+		   w.WriteHeader(http.StatusInternalServerError)
+		   w.Write([]byte("Internal Server Error\n"))
+               }
+           }()
+           h.ServeHTTP(w, req)
+       })
+   }
 }
