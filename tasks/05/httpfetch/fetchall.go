1be7410b218dc33ca793ff192e684597d95d4f4e
Roman <roman@kali.linux-kali>
2022-05-12 21:00:59 -0400

    cosmetic

--- tasks/05/httpfetch/fetchall.go
+++ tasks/05/httpfetch/fetchall.go
@@ -1,10 +1,7 @@
 package httpfetch
 
 import (
-	"net/http"
 	"sync"
-	"bytes"
-	"log"
 )
 
 type Request struct {
@@ -19,37 +16,5 @@ type Result struct {
 }
 
 func FetchAll(c *http.Client, requests []Request) []Result {
-	var wg sync.WaitGroup
-	quantityReq := len(requests)
-	results := make([]Result, quantityReq)
-
-	handleRequest := func(i int) {
-		req, err := http.NewRequest(
-			requests[i].Method, 
-			requests[i].URL, 
-			bytes.NewReader(requests[i].Body),
-		)
-		if err != nil {
-			log.Printf("can't create a request struct: %v", err)
-			return
-		}
-		response, err := c.Do(req)
-		if err != nil {
-			results[i].Error = err
-		}else {
-			results[i] = Result {
-				StatusCode: 	response.StatusCode,
-				Error:		err,
-			}
-		}
-		return
-
-		defer response.Body.Close()
-	}
-	for i := 0; i < quantityReq; i++ {
-		go handleRequest(i)
-	}
-	wg.Wait()
-	return results
-
+	return nil
 }
