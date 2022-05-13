54d76a49ba4d9f970226544a04a0e70070e35069
Roman <roman@kali.linux-kali>
2022-05-12 20:45:11 -0400

    fetchal

--- tasks/05/httpfetch/fetchall.go
+++ tasks/05/httpfetch/fetchall.go
@@ -23,26 +23,30 @@ func FetchAll(c *http.Client, requests []Request) []Result {
 	quantityReq := len(requests)
 	wg.Add(quantityReq)
 	results := make([]Result, quantityReq)
+
+	// main worker to goroutine that create request and fill results array
+	requestWorker := func(i int) {
+		defer wg.Done()
+		reqHttp, err := http.NewRequest(
+			requests[i].Method, requests[i].URL, bytes.NewReader(requests[i].Body),
+		)
+		if err != nil {
+			results[i].Error = err
+			return
+		}
+
+		response, err := c.Do(reqHttp)
+		if err != nil {
+			results[i].Error = err
+			return
+		}
+		results[i].StatusCode = response.StatusCode
+		defer response.Body.Close()
+	}
+
 	for i := 0; i < quantityReq; i++ {
-		go requestWorker(c, &wg, &requests[i], &results[i])
+		go requestWorker(i)
 	}
 	wg.Wait()
 	return results
-}
-
-func requestWorker(c *http.Client, wg *sync.WaitGroup, request *Request, result *Result) {
-	reqHttp, err := http.NewRequest(request.Method, request.URL, bytes.NewReader(request.Body))
-	if err != nil {
-		return
-	}
-
-	response, err := c.Do(reqHttp)
-	if err != nil {
-		result.Error = err
-		return
-	}
-
-	defer wg.Done()
-	defer response.Body.Close()
-	result.StatusCode = response.StatusCode
 }
